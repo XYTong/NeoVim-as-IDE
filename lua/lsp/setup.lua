0f3d603932cc4740b1347aa685d339d95844fd1a
XYTong <xiangyu.tong.cs@gmail.com>
2021-12-27 10:05:58 +0100

    nvim as ide

---
+++ lua/lsp/setup.lua
@@ -0,0 +1,98 @@
+local lsp_installer = require "nvim-lsp-installer"
+
+-- 安装列表
+-- https://github.com/williamboman/nvim-lsp-installer#available-lsps
+-- { key: 语言 value: 配置文件 }
+local servers = {
+  sumneko_lua = require "lsp.lua", -- /lua/lsp/lua.lua
+  rust_analyzer = require "lsp.rust",
+  --html = require "lsp.html",
+  -- html = {},
+  -- jsonls = {},
+  -- tsserver = {}
+}
+-- 自动安装 LanguageServers
+for name, _ in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found then
+    if not server:is_installed() then
+      print("Installing " .. name)
+      server:install()
+    end
+  end
+end
+
+
+lsp_installer.on_server_ready(function(server)
+  local opts = servers[server.name]
+  if opts then
+    opts.on_attach = function(_, bufnr)
+      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+      -- 绑定快捷键
+      require('keybindings').maplsp(buf_set_keymap)
+    end
+    opts.flags = {
+      debounce_text_changes = 150,
+    }
+
+    if server.name == "rust_analyzer" then
+      -- Initialize the LSP via rust-tools instead
+      require("rust-tools").setup {
+        -- The "server" property provided in rust-tools setup function are the
+        -- settings rust-tools will provide to lspconfig during init.            --
+        -- We merge the necessary settings from nvim-lsp-installer (server:get_default_options())
+        -- with the user's own settings (opts).
+        print(vim.inspect(opts)),
+        server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+      }
+      server:attach_buffers()
+    else
+      server:setup(opts)
+    end
+  end
+end)
+--[[
+local nvim_lsp = require 'lspconfig'
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  },
+}
+local on_attach = function(client, bufnr)
+
+
+local cpp_server = {'clangd', 'pyright', 'html'}
+
+-- Set settings for language servers below
+--
+-- tsserver settings
+local ts_settings = function(client)
+  client.resolved_capabilities.document_formatting = false
+  ts_settings(client)
+end
+
+for _, lsp in ipairs(cpp_server) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    ts_settings = ts_settings,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+end
+  end--]]
---
+++ lua/plugins.lua
@@ -0,0 +1,120 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- gruvbox theme
+    use {
+        "ellisonleao/gruvbox.nvim",
+        requires = {"rktjmp/lush.nvim"}
+    }
+  -------------------------- plugins -------------------------------------------
+
+  -- nvim-tree
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+  -- bufferline
+  use {
+    'akinsho/bufferline.nvim',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+  -- treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  -- Comment
+  use 'numToStr/Comment.nvim'
+
+  -- nvim-autopairs
+  use 'windwp/nvim-autopairs'
+  -------------------------- lsp -------------------------------------------
+
+  -- lspconfig
+  use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
+  -- nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
+  use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
+  use 'hrsh7th/cmp-path'     -- { name = 'path' }
+  use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
+  use 'hrsh7th/nvim-cmp'
+  -- vsnip
+  use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
+  use 'hrsh7th/vim-vsnip'
+  use 'rafamadriz/friendly-snippets'
+  -- lspkind
+  use 'onsails/lspkind-nvim'
+  -- use 'github/copilot.vim'
+
+  use 'simrat39/rust-tools.nvim'
+  --------------------------- colorscheme ------------------------------------
+
+  -- gruvbox
+  use {
+    "ellisonleao/gruvbox.nvim",
+    requires = {"rktjmp/lush.nvim"}
+  }
+  -- zephyr
+  use 'glepnir/zephyr-nvim'
+  -- nord
+  use 'shaunsingh/nord.nvim'
+  -- onedark
+  use 'ful1e5/onedark.nvim'
+  ------------------------------------
+  --
+  -- which-key
+  use {
+  "folke/which-key.nvim",
+  config = function()
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+  -- telescope
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-lua/plenary.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/plenary.nvim'}}
+  }
+
+  -- use {
+  --   "folke/trouble.nvim",
+  --   requires = "kyazdani42/nvim-web-devicons",
+  --   config = function()
+  --     require("trouble").setup {
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       -- refer to the configuration section below
+  --     }
+  --   end
+  -- }
+
+
+
+  -- rust-tools
+  -- use 'simrat39/rust-tools.nvim'
+  -- nvim-cmp
+  -- use 'hrsh7th/cmp-nvim-lsp'
+  -- use 'hrsh7th/cmp-buffer'
+  -- use 'hrsh7th/cmp-path'
+  -- use 'hrsh7th/cmp-cmdline'
+  -- use 'hrsh7th/nvim-cmp'
+  -- use 'hrsh7th/cmp-vsnip'
+  -- vsnip
+  -- use 'hrsh7th/vim-vsnip'
+  -- use "rafamadriz/friendly-snippets"
+  -- use 'nvim-autopairs'
+  -- use "windwp/nvim-autopairs"
+  -- surround
+  -- use "blackCauldron7/surround.nvim"
+  -- nvim-coloizer
+  use 'norcalli/nvim-colorizer.lua'
+  -- saga
+  use { 'tami5/lspsaga.nvim' }  -- nightly
+end)
+
