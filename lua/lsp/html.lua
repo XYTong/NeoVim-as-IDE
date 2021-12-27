1eb1087e5f1d2f25c2a757f9e92ee7b75f56b76d
XYTong <xiangyu.tong.cs@gmail.com>
2021-12-27 11:07:05 +0100

    update

--- lua/lsp/html.lua
+++ lua/lsp/html.lua
@@ -1,7 +1,1 @@
---Enable (broadcasting) snippet capability for completion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-require'lspconfig'.html.setup {
-  capabilities = capabilities,
-}
+return {}
